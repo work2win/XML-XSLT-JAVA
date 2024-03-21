df97a3f1d176058aec2ad85a6a6fdc1a5b683069
work2win <worknpeace2win@gmail.com>
2024-03-21 15:32:48 -0400

    java class

---
+++ javacode/src/main/java/com/work2win/jaxp/JaxpParsing.java
@@ -0,0 +1,40 @@
+package com.work2win.jaxp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class JaxpParsing {
+	
+	public static void main(String[] args) {
+		
+
+		String workingDir = System.getProperty("user.dir");
+		System.out.println("Current working directory : " + workingDir);
+		
+		File file = new File(workingDir, "file.xml"); 
+		String absolutePath = file.getAbsolutePath();
+		System.out.println("File path : " + absolutePath);
+		
+		try {
+			
+		InputStream ins = new FileInputStream(file);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();	    
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		
+		Document doc = dBuilder.parse(ins);
+		doc.getDocumentElement().normalize();
+		
+		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
