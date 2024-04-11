11ca08c554d28b4114dfc39cf9ad951df2f1af86
work2win <worknpeace2win@gmail.com>
2024-04-10 20:53:24 -0400

    stream

---
+++ javacode/src/main/java/com/work2win/SampleStream.java
@@ -0,0 +1,33 @@
+package com.work2win;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.google.common.collect.Lists;
+
+
+
+public class SampleStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stream<Integer> stream;
+		List<Integer> li = Lists.newArrayList(1,2,3,4,5);
+		List square = li.stream().map(i->i*i).collect(Collectors.toList());
+		System.out.println(square);
+		Map<Boolean, List<Integer>> m = li.stream().collect(Collectors.partitioningBy(i -> i%2 == 0));
+		System.out.println(m);
+		Set<Entry<Boolean, List<Integer>>> e = m.entrySet();
+		for(Entry<Boolean, List<Integer>> en : e) {
+			if(en.getKey())
+				System.out.println("even"+en.getValue());
+			else
+				System.out.println("odd"+en.getValue());
+		}
+	}
+
+}
